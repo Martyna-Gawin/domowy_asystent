47e08954ddf5a8306e5d793f9ff7a7eb97da9ad9
Martyna Gawin <martynagawin1@gmail.com>
2022-06-07 18:41:31 +0200

    Create more screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'dart:math';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,30 +14,89 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 17, 57, 89),
-        body: Center(
-          child: Column(
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 17, 57, 89),
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const CircleAvatar(
               backgroundImage: AssetImage('images/domek1.png'),
               radius: 100,
             ),
-          const SizedBox(height: 50),
-          Text('Domowy Asystent',
-          style: GoogleFonts.cookie(
-          fontSize: 40,
-          color: Colors.white
-          ),
-          ),
+            const SizedBox(height: 50),
+            Text(
+              'Domowy Asystent',
+              style: GoogleFonts.cookie(fontSize: 40, color: Colors.white),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              child: const Text('Zaloguj'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => const SecondPage(),
+                  ),
+                );
+              },
+            ),
           ],
-          ),
         ),
       ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color.fromARGB(255, 17, 57, 89),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text('Wejdź'),
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (_) => const ThirdPage(),
+                ),);
+              },
+            )
+          ],
+        )));
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  const ThirdPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 17, 57, 89),
+      
     );
   }
 }
