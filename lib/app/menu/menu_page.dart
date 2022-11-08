a42616a57e423be292963b8f0113f77502c4b98b
Martyna Gawin <martynagawin1@gmail.com>
2022-11-08 14:39:27 +0100

    Organize files

--- lib/app/menu/menu_page.dart
+++ lib/app/menu/menu_page.dart
@@ -3,26 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'dart:math';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
-    );
-  }
-}
-
 
 
 class MenuPage extends StatelessWidget {
