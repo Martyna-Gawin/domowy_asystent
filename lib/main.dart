9f080ac02f2fc68e5edb55e7359f2f3df3853b8a
Martyna Gawin <martynagawin1@gmail.com>
2022-11-08 14:32:37 +0100

    Change look

--- lib/main.dart
+++ lib/main.dart
@@ -13,115 +13,148 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: FirstPage(),
+      home: const HomePage(),
     );
   }
 }
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({
+class HomePage extends StatelessWidget {
+  const HomePage({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 17, 57, 89),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment(0.8, 1),
+          colors: <Color>[
+            Color.fromARGB(255, 27, 68, 7),
+            Color.fromARGB(255, 38, 87, 13),
+            Color.fromARGB(255, 54, 112, 25),
+            Color.fromARGB(255, 66, 130, 34),
+            Color.fromARGB(255, 83, 154, 48),
+            Color.fromARGB(255, 128, 202, 90),
+            Color.fromARGB(255, 172, 243, 136),
+          ], // Gradient from https://learnui.design/tools/gradient-generator.html
+          tileMode: TileMode.mirror,
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.house, size: 100, color: Colors.white),
+          const SizedBox(height: 30),
+          Text(
+            'Domowy Asystent',
+            style: GoogleFonts.cookie(fontSize: 60, color: Colors.white),
+          ),
+          const SizedBox(height: 120),
+          IconButton(
+            icon: const Icon(Icons.login, size: 40, color: Colors.white),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => const LoginPage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment(0.8, 1),
+              colors: <Color>[
+                Color.fromARGB(255, 27, 68, 7),
+                Color.fromARGB(255, 38, 87, 13),
+                Color.fromARGB(255, 54, 112, 25),
+                Color.fromARGB(255, 66, 130, 34),
+                Color.fromARGB(255, 83, 154, 48),
+                Color.fromARGB(255, 128, 202, 90),
+                Color.fromARGB(255, 172, 243, 136),
+              ],
+              tileMode: TileMode.mirror,
+            ),
+          ),
+          child: ElevatedButton(
+            child: const Text('Dalej'),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => const MenuPage()),
+              );
+            },
+          )),
+    );
+  }
+}
+
+class MenuPage extends StatelessWidget {
+  const MenuPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment(0.8, 1),
+            colors: <Color>[
+              Color.fromARGB(255, 27, 68, 7),
+              Color.fromARGB(255, 38, 87, 13),
+              Color.fromARGB(255, 54, 112, 25),
+              Color.fromARGB(255, 66, 130, 34),
+              Color.fromARGB(255, 83, 154, 48),
+              Color.fromARGB(255, 128, 202, 90),
+              Color.fromARGB(255, 172, 243, 136),
+            ],
+            tileMode: TileMode.mirror,
+          ),
+        ),
+        child: ListView(
           children: [
-            const CircleAvatar(
-              backgroundImage: AssetImage('images/domek1.png'),
-              radius: 100,
-            ),
-            const SizedBox(height: 50),
-            Text(
-              'Domowy Asystent',
-              style: GoogleFonts.cookie(fontSize: 40, color: Colors.white),
-            ),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              child: const Text('Zaloguj'),
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) => const SecondPage(),
-                  ),
-                );
-              },
+            Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  color: Colors.green,
+                ),
+              ],
             ),
           ],
         ),
       ),
     );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  const SecondPage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Color.fromARGB(255, 17, 57, 89),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: Text('Wejdź'),
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (_) => const ThirdPage(),
-                ),);
-              },
-            )
-          ],
-        )));
-  }
-}
-
-class ThirdPage extends StatelessWidget {
-  const ThirdPage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 17, 57, 89),
-     body: Column(
-       mainAxisAlignment: MainAxisAlignment.spaceAround,
-       children: [
-         Text('Budżet',
-         style:GoogleFonts.poppins(
-         fontSize: 20,
-         color: Colors.white
-          ) ),
-         Text('Kalendarz',
-         style:GoogleFonts.poppins(
-         fontSize: 20,
-         color: Colors.white
-          )),
-         Text('Lista zakupów',
-         style:GoogleFonts.poppins(
-         fontSize: 20,
-         color: Colors.white
-         )),
-         Text('Długi',
-         style:GoogleFonts.poppins(
-         fontSize: 20,
-         color: Colors.white
-          )),
-       ],
-     )
-      );
-    
   }
 }
