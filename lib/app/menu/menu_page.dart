17c056b1bdaea6922eae518338f6130f797689e4
Martyna Gawin <martynagawin1@gmail.com>
2022-11-27 13:33:08 +0100

    Add Safe Area

--- lib/app/menu/menu_page.dart
+++ lib/app/menu/menu_page.dart
@@ -1,7 +1,5 @@
-import 'package:domowy_asystent/app/home/home_page.dart';
+import 'package:domowy_asystent/app/category_widget/category_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'dart:math';
 
 class MenuPage extends StatelessWidget {
   const MenuPage({
@@ -30,75 +28,25 @@ class MenuPage extends StatelessWidget {
             tileMode: TileMode.mirror,
           ),
         ),
-        child: ListView(
-          children: [
-            Center(
-              child: Column(
-                children: const [
-                  CategoryWidget(
-                    icon: Icon(Icons.calendar_month, color: Colors.white),
-                    title: ('Wydarzenia'),
-                  ),
-                  CategoryWidget(
-                    icon: Icon(Icons.list_sharp, color: Colors.blue),
-                    title: ('Lista'),
-                  ),
-                  CategoryWidget(
-                    icon: Icon(Icons.wallet, color: Colors.brown),
-                    title: ('Portfel'),
-                  ),
-                ],
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.person, color: Colors.white),
+                      iconSize: 30,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CategoryWidget extends StatelessWidget {
-  const CategoryWidget({
-    Key? key,
-    required this.icon,
-    required this.title,
-  }) : super(key: key);
-
-  final Widget icon;
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Container(
-        width: 300,
-        height: 200,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(32),
-          color: const Color.fromARGB(255, 192, 195, 192),
-          boxShadow: [
-            BoxShadow(
-              color: const Color.fromARGB(255, 87, 84, 84).withOpacity(0.5),
-              spreadRadius: 7,
-              blurRadius: 3,
-              offset: const Offset(0, 3),
-            ),
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-              onPressed: () {},
-              icon: icon,
-              iconSize: 70,
-            ),
-            Text(
-              title,
-              style: const TextStyle(fontSize: 22),
-            ),
-          ],
+              const CategoryWidget()
+            ],
+          ),
         ),
       ),
     );
